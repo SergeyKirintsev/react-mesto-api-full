e3c5617492719cb85f318abdb8fbac8c7a0f1617
SergeyKirintsev <kserg80@gmail.com>
2021-08-19 15:46:33 +0300

    доработать cors

--- backend/app.js
+++ backend/app.js
@@ -11,18 +11,18 @@ const errorsHandler = require('./errors/errorsHandler');
 const { linkRegExp } = require('./utils/utils');
 const { NotFoundError } = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// app.use(cors);
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(requestLogger);
+
+app.use(cors);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,9 +1,9 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
+  'http://localhost:3000',
+  'http://kirser.nomoredomains.club/',
+  'https://kirser.nomoredomains.club/',
 ];
 
 // eslint-disable-next-line consistent-return
@@ -12,16 +12,11 @@ module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  console.log('cors...', req.headers);
-  console.log('cors...', origin);
-
   if (allowedCors.includes(origin)) {
-    console.log('allowedCors');
     res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
-    console.log('OPTIONS');
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
